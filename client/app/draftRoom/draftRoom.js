babda2c7ae7ae6d491185bd9fce2f6c413b47a59
brettkan <bkfighton@gmail.com>
2015-06-15 20:11:00 -0700

    Added functionality to draft player in DraftRoomController, remove from available players, add to drafted players

--- client/app/draftRoom/draftRoom.js
+++ client/app/draftRoom/draftRoom.js
@@ -1,16 +1,17 @@
 angular.module('FFDrafter.draftRoom', [])
 
 .controller('DraftRoomController', function ($scope, $location, Players) {
-  // Your code here
-  // $scope.link = $scope.link || {data: null};
+  
+  $scope.availablePlayers = {};
 
-  // $scope.addLink = function(url){
-  //   Links.shortenUrl(url).then(function(data) {
-  //     $scope.link.data = data;
-  //   });
-  // };
+  $scope.draftedPlayers = [];
 
-  $scope.data = {};
+  $scope.tracker = {
+    playerPick: 1,
+    round: 1,
+    numberOfPlayers: 12,
+    numberOfRounds: 15
+  };
 
   $scope.addAllPlayers = function(playerObj){
     Players.getPlayerList().then(function(data) {
@@ -18,6 +19,58 @@ angular.module('FFDrafter.draftRoom', [])
     });
   };
 
-  $scope.addAllPlayers($scope.data);
+  $scope.draftPlayer = function(availablePlayersIndex) {
+    console.log('Player drafted');
+
+    $scope.addToDrafted(availablePlayersIndex);
+    $scope.removeFromAvailable(availablePlayersIndex);
+    $scope.incrementPick();
+  };
+
+  $scope.addToDrafted = function(index) {
+    var current = $scope.availablePlayers.players[index];
+    var currentRound = $scope.tracker.round;
+    var currentPlayer = $scope.tracker.playerPick - 1;
+
+    if (!$scope.draftedPlayers[currentRound]) {
+      $scope.draftedPlayers[currentRound] = [];
+    }
+
+    $scope.draftedPlayers[currentRound][currentPlayer] = current;
+  };
+
+  $scope.removeFromAvailable = function(index) {
+    $scope.availablePlayers.players.splice(index, 1);
+  };
+
+  $scope.incrementPick = function() {
+    // In odd rounds, player pick should go up in a snake draft
+    if ($scope.tracker.round % 2 === 1) {
+      $scope.tracker.playerPick++;
+
+      // If last player has picked, increment round, last player picks again
+      if ($scope.tracker.playerPick > $scope.tracker.numberOfPlayers) {
+        $scope.tracker.round++;
+        $scope.tracker.playerPick--;
+      }
+    } else { // In even rounds, player pick should go back down in a snake draft
+      $scope.tracker.playerPick--;
+
+      // If first player has picked, increment round, first player picks again
+      if ($scope.tracker.playerPick < 1) {
+        $scope.tracker.round++;
+        $scope.tracker.playerPick++;
+      }
+    }
+  };
+  
+
+
+
+
+
+
+
+  $scope.addAllPlayers($scope.availablePlayers);
 
 });
